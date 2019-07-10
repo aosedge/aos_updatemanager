84addf0be5515f6460f578709a01475e98fbed22
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2019-07-10 19:47:47 +0300

    [updatemanager] Update main function to create all needed instances
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Oleh Mykytiuk <oleh_mykytiuk@epam.com>

--- updatemanager.go
+++ updatemanager.go
@@ -4,16 +4,34 @@ import (
 	"flag"
 	"os"
 	"os/signal"
+	"path"
 	"syscall"
+
+	"gitpct.epam.com/epmd-aepr/aos_updatemanager/updatehandler"
 
 	log "github.com/sirupsen/logrus"
 
 	"gitpct.epam.com/epmd-aepr/aos_updatemanager/config"
+	"gitpct.epam.com/epmd-aepr/aos_updatemanager/database"
 	"gitpct.epam.com/epmd-aepr/aos_updatemanager/umserver"
 )
 
+/*******************************************************************************
+ * Consts
+ ******************************************************************************/
+
+const dbFileName = "updatemanager.db"
+
+/*******************************************************************************
+ * Vars
+ ******************************************************************************/
+
 // GitSummary provided by govvv at compile-time
 var GitSummary string
+
+/*******************************************************************************
+ * Init
+ ******************************************************************************/
 
 func init() {
 	log.SetFormatter(&log.TextFormatter{
@@ -22,6 +40,23 @@ func init() {
 		FullTimestamp:    true})
 	log.SetOutput(os.Stdout)
 }
+
+/*******************************************************************************
+ * Private
+ ******************************************************************************/
+
+func cleanup(workingDir, dbFile string) {
+	log.Debug("System cleanup")
+
+	log.WithField("file", dbFile).Debug("Delete DB file")
+	if err := os.RemoveAll(dbFile); err != nil {
+		log.Fatalf("Can't cleanup database: %s", err)
+	}
+}
+
+/*******************************************************************************
+ * Main
+ ******************************************************************************/
 
 func main() {
 	// Initialize command line flags
@@ -39,14 +74,36 @@ func main() {
 
 	log.WithFields(log.Fields{"configFile": *configFile, "version": GitSummary}).Info("Start update manager")
 
-	config, err := config.New(*configFile)
+	cfg, err := config.New(*configFile)
 	if err != nil {
 		log.Fatalf("Can' open config file: %s", err)
 	}
 
-	server, err := umserver.New(config)
+	// Create DB
+	dbFile := path.Join(cfg.WorkingDir, dbFileName)
+
+	db, err := database.New(dbFile)
 	if err != nil {
-		log.Fatalf("Can't create ws server: %s", err)
+		if err == database.ErrVersionMismatch {
+			log.Warning("Unsupported database version")
+			cleanup(cfg.WorkingDir, dbFile)
+			db, err = database.New(dbFile)
+		}
+
+		if err != nil {
+			log.Fatalf("Can't create database: %s", err)
+		}
+	}
+
+	updater, err := updatehandler.New(cfg, db)
+	if err != nil {
+		log.Fatalf("Can't create updater: %s", err)
+	}
+	defer updater.Close()
+
+	server, err := umserver.New(cfg, updater)
+	if err != nil {
+		log.Fatalf("Can't create UM server: %s", err)
 	}
 	defer server.Close()
 
