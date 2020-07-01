fee961b5e1dacd073c1958e4871c3c82e75ad54e
Yevgen Abramov <Yevgen_Abramov@epam.com>
2020-07-01 16:11:18 +0300

    Remove build tags for plugins
    Signed-off-by: Yevgen Abramov <Yevgen_Abramov@epam.com>

--- modules/sshmodule/register.go
+++ modules/sshmodule/register.go
@@ -15,8 +15,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build WITH_SSHMODULE
-
 package sshmodule
 
 import "aos_updatemanager/updatehandler"
