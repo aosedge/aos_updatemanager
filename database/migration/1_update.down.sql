6e3e7480d93a447815d89890506befcc666e4f74
Yevgen Abramov <Yevgen_Abramov@epam.com>
2021-03-30 16:30:30 +0300

    [migration] Update migration scripts
    Signed-off-by: Yevgen Abramov <Yevgen_Abramov@epam.com>

--- database/migration/1_update.down.sql
+++ database/migration/1_update.down.sql
@@ -1,2 +1,12 @@
 ALTER TABLE config ADD version INTEGER;
 UPDATE config SET version = 5;
+
+CREATE TABLE modules_new (id TEXT NOT NULL PRIMARY KEY, state TEXT);
+
+INSERT INTO modules_new (id, state)
+SELECT id, state
+FROM modules;
+
+DROP TABLE modules;
+
+ALTER TABLE modules_new RENAME TO modules;
--- database/migration/1_update.up.sql
+++ database/migration/1_update.up.sql
@@ -7,3 +7,19 @@ FROM config;
 DROP TABLE config;
 
 ALTER TABLE config_new RENAME TO config;
+
+DROP TABLE modules_data;
+DROP TABLE certificates;
+
+CREATE TABLE modules_new (id TEXT NOT NULL PRIMARY KEY,
+						  vendorVersion TEXT,
+			              aosVersion INTEGER,
+			              state TEXT);
+
+INSERT INTO modules_new (id, state)
+SELECT id, state
+FROM modules;
+
+DROP TABLE modules;
+
+ALTER TABLE modules_new RENAME TO modules;
