4e0aad76dba37a26c138f114e7eebb55ee8aa60d
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2022-12-13 21:35:11 +0200

    [vendor] Update to use new aos_common
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- vendor/github.com/aoscloud/aos_common/aostypes/aostypes.go
+++ vendor/github.com/aoscloud/aos_common/aostypes/aostypes.go
@@ -19,11 +19,31 @@ package aostypes
 
 import (
 	"encoding/json"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
 
+	imagespec "github.com/opencontainers/image-spec/specs-go/v1"
+
 	"github.com/aoscloud/aos_common/aoserrors"
+)
+
+/***********************************************************************************************************************
+ * Consts
+ **********************************************************************************************************************/
+
+// nolint
+const (
+	alternativePattern = `^P(((?P<year>\d+)-)((?P<month>\d+)-)((?P<day>\d+)))?(T((?P<hour>\d+):)((?P<minute>\d+):)(?P<second>\d+))?$`
+	canonicPattern     = `^P((?P<year>\d+)Y)?((?P<month>\d+)M)?((?P<week>\d+)W)?((?P<day>\d+)D)?(T((?P<hour>\d+)H)?((?P<minute>\d+)M)?((?P<second>\d+)S)?)?$`
+)
+
+const (
+	dayDuration   = 24 * time.Hour
+	weekDuration  = 7 * dayDuration
+	yearDuration  = 365*dayDuration + 6*time.Hour
+	monthDuration = yearDuration / 12
 )
 
 /***********************************************************************************************************************
@@ -40,20 +60,26 @@ type Time struct {
 	time.Time
 }
 
-// AlertRule describes alert rule.
-type AlertRule struct {
+// AlertRuleParam describes alert rule.
+type AlertRuleParam struct {
 	MinTimeout   Duration `json:"minTimeout"`
 	MinThreshold uint64   `json:"minThreshold"`
 	MaxThreshold uint64   `json:"maxThreshold"`
 }
 
-// ServiceAlertRules define service monitoring alerts rules.
-type ServiceAlertRules struct {
-	RAM        *AlertRule `json:"ram,omitempty"`
-	CPU        *AlertRule `json:"cpu,omitempty"`
-	UsedDisk   *AlertRule `json:"usedDisk,omitempty"`
-	InTraffic  *AlertRule `json:"inTraffic,omitempty"`
-	OutTraffic *AlertRule `json:"outTraffic,omitempty"`
+// PartitionAlertRuleParam describes alert rule.
+type PartitionAlertRuleParam struct {
+	AlertRuleParam
+	Name string `json:"name"`
+}
+
+// AlertRules define service monitoring alerts rules.
+type AlertRules struct {
+	RAM        *AlertRuleParam           `json:"ram,omitempty"`
+	CPU        *AlertRuleParam           `json:"cpu,omitempty"`
+	UsedDisks  []PartitionAlertRuleParam `json:"usedDisks,omitempty"`
+	InTraffic  *AlertRuleParam           `json:"inTraffic,omitempty"`
+	OutTraffic *AlertRuleParam           `json:"outTraffic,omitempty"`
 }
 
 // FileSystemMount specifies a mount instructions.
@@ -87,12 +113,117 @@ type ResourceInfo struct {
 	Hosts  []Host            `json:"hosts,omitempty"`
 }
 
-// BoardConfig board configuration.
-type BoardConfig struct {
-	FormatVersion uint64         `json:"formatVersion"`
-	VendorVersion string         `json:"vendorVersion"`
-	Devices       []DeviceInfo   `json:"devices"`
-	Resources     []ResourceInfo `json:"resources"`
+// NodeConfig node configuration.
+type NodeUnitConfig struct {
+	NodeType  string         `json:"nodeType"`
+	Devices   []DeviceInfo   `json:"devices,omitempty"`
+	Resources []ResourceInfo `json:"resources,omitempty"`
+	Labels    []string       `json:"labels,omitempty"`
+	Priority  uint32         `json:"priority,omitempty"`
+}
+
+// UnitConfig board configuration.
+type UnitConfig struct {
+	FormatVersion uint64           `json:"formatVersion"`
+	VendorVersion string           `json:"vendorVersion"`
+	Nodes         []NodeUnitConfig `json:"nodes"`
+}
+
+// ServiceInfo service info.
+type ServiceInfo struct {
+	VersionInfo
+	ID         string `json:"id"`
+	ProviderID string `json:"providerId"`
+	GID        uint32 `json:"gid"`
+	URL        string `json:"url"`
+	Sha256     []byte `json:"sha256"`
+	Sha512     []byte `json:"sha512"`
+	Size       uint64 `json:"size"`
+}
+
+// LayerInfo layer info.
+type LayerInfo struct {
+	VersionInfo
+	ID     string `json:"id"`
+	Digest string `json:"digest"`
+	URL    string `json:"url"`
+	Sha256 []byte `json:"sha256"`
+	Sha512 []byte `json:"sha512"`
+	Size   uint64 `json:"size"`
+}
+
+// VersionInfo common version structure.
+type VersionInfo struct {
+	AosVersion    uint64 `json:"aosVersion"`
+	VendorVersion string `json:"vendorVersion"`
+	Description   string `json:"description"`
+}
+
+// InstanceIdent instance identification information.
+type InstanceIdent struct {
+	ServiceID string `json:"serviceId"`
+	SubjectID string `json:"subjectId"`
+	Instance  uint64 `json:"instance"`
+}
+
+// InstanceInfo instance information to start it.
+type InstanceInfo struct {
+	InstanceIdent
+	UID         uint32 `json:"uid"`
+	Priority    uint64 `json:"priority"`
+	StoragePath string `json:"storagePath"`
+	StatePath   string `json:"statePath"`
+}
+
+// ServiceManifest Aos service manifest.
+type ServiceManifest struct {
+	imagespec.Manifest
+	AosService *imagespec.Descriptor `json:"aosService,omitempty"`
+}
+
+// ServiceDevice struct with service divices rules.
+type ServiceDevice struct {
+	Name        string `json:"name"`
+	Permissions string `json:"permissions"`
+}
+
+// ServiceQuotas service quotas representation.
+type ServiceQuotas struct {
+	CPULimit      *uint64 `json:"cpuLimit,omitempty"`
+	RAMLimit      *uint64 `json:"ramLimit,omitempty"`
+	PIDsLimit     *uint64 `json:"pidsLimit,omitempty"`
+	NoFileLimit   *uint64 `json:"noFileLimit,omitempty"`
+	TmpLimit      *uint64 `json:"tmpLimit,omitempty"`
+	StateLimit    *uint64 `json:"stateLimit,omitempty"`
+	StorageLimit  *uint64 `json:"storageLimit,omitempty"`
+	UploadSpeed   *uint64 `json:"uploadSpeed,omitempty"`
+	DownloadSpeed *uint64 `json:"downloadSpeed,omitempty"`
+	UploadLimit   *uint64 `json:"uploadLimit,omitempty"`
+	DownloadLimit *uint64 `json:"downloadLimit,omitempty"`
+}
+
+// RunParameters service startup parameters.
+type RunParameters struct {
+	StartInterval   Duration `json:"startInterval,omitempty"`
+	StartBurst      uint     `json:"startBurst,omitempty"`
+	RestartInterval Duration `json:"restartInterval,omitempty"`
+}
+
+// ServiceConfig Aos service configuration.
+type ServiceConfig struct {
+	Created            time.Time                    `json:"created"`
+	Author             string                       `json:"author"`
+	Hostname           *string                      `json:"hostname,omitempty"`
+	Runner             string                       `json:"runner"`
+	Sysctl             map[string]string            `json:"sysctl,omitempty"`
+	ServiceTTL         *uint64                      `json:"serviceTtl,omitempty"`
+	Quotas             ServiceQuotas                `json:"quotas"`
+	AllowedConnections map[string]struct{}          `json:"allowedConnections,omitempty"`
+	Devices            []ServiceDevice              `json:"devices,omitempty"`
+	Resources          []string                     `json:"resources,omitempty"`
+	Permissions        map[string]map[string]string `json:"permissions,omitempty"`
+	AlertRules         *AlertRules                  `json:"alertRules,omitempty"`
+	RunParameters      RunParameters                `json:"runParameters,omitempty"`
 }
 
 /***********************************************************************************************************************
@@ -180,16 +311,78 @@ func (d *Duration) UnmarshalJSON(b []byte) (err error) {
 		return nil
 
 	case string:
-		duration, err := time.ParseDuration(value)
-		if err != nil {
-			return aoserrors.Wrap(err)
-		}
+		if !strings.HasPrefix(value, "P") {
+			duration, err := time.ParseDuration(value)
+			if err != nil {
+				return aoserrors.Wrap(err)
+			}
 
-		d.Duration = duration
+			d.Duration = duration
+		} else {
+			duration, err := parseISO8601Duration(value)
+			if err != nil {
+				return aoserrors.Wrap(err)
+			}
+
+			d.Duration = duration
+		}
 
 		return nil
 
 	default:
 		return aoserrors.Errorf("invalid duration value: %v", value)
 	}
+}
+
+func parseISO8601Duration(value string) (time.Duration, error) {
+	var (
+		patternStr = canonicPattern
+		match      []string
+		d          time.Duration
+	)
+
+	if strings.Contains(value, "-") || strings.Contains(value, ":") {
+		patternStr = alternativePattern
+	}
+
+	pattern := regexp.MustCompile(patternStr)
+
+	if !pattern.MatchString(value) {
+		return d, aoserrors.New("could not parse duration string")
+	}
+
+	match = pattern.FindStringSubmatch(value)
+
+	for i, name := range pattern.SubexpNames() {
+		part := match[i]
+		if i == 0 || name == "" || part == "" {
+			continue
+		}
+
+		val, err := strconv.Atoi(part)
+		if err != nil {
+			return d, aoserrors.Wrap(err)
+		}
+
+		switch name {
+		case "year":
+			d += time.Duration(val) * yearDuration
+		case "month":
+			d += time.Duration(val) * monthDuration
+		case "week":
+			d += time.Duration(val) * weekDuration
+		case "day":
+			d += time.Duration(val) * dayDuration
+		case "hour":
+			d += time.Duration(val) * time.Hour
+		case "minute":
+			d += time.Duration(val) * time.Minute
+		case "second":
+			d += time.Duration(val) * time.Second
+		default:
+			return d, aoserrors.Errorf("unknown field %s", name)
+		}
+	}
+
+	return d, nil
 }
