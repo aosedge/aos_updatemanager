b739fec0ace13cd9ec7ba35ceb1afe46275742f1
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2023-02-02 14:03:58 +0200

    [updatemodules] Implement bootparams package to get kernel boot args
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ updatemodules/partitions/utils/bootparams/bootparams.go
@@ -0,0 +1,182 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright (C) 2023 Renesas Electronics Corporation.
+// Copyright (C) 2023 EPAM Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bootparams
+
+import (
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/aoscloud/aos_common/aoserrors"
+)
+
+/***********************************************************************************************************************
+ * Types
+ **********************************************************************************************************************/
+
+type Handler struct {
+	params map[string]string
+}
+
+/***********************************************************************************************************************
+ * consts
+ **********************************************************************************************************************/
+
+const (
+	autoMode     = "auto"
+	bootArgsMode = "bootargs"
+	manualMode   = "manual"
+)
+
+const (
+	rootParam            = "root"
+	aosUpdateBootPartA   = "aosupdate.boot.parta"
+	aosUpdateBootPartB   = "aosupdate.boot.partb"
+	aosUpdateBootEnvPart = "aosupdate.boot.envPart"
+)
+
+/***********************************************************************************************************************
+ * Vars
+ **********************************************************************************************************************/
+
+// BootParamsPath path to boot params file.
+var BootParamsPath = "/proc/cmdline" //nolint:gochecknoglobals // Used in unit tests to override path
+
+var rootExp = regexp.MustCompile("[[:digit:]]+$")
+
+/***********************************************************************************************************************
+ * Public
+ **********************************************************************************************************************/
+
+// New creates new parser instance.
+func New() (*Handler, error) {
+	data, err := os.ReadFile(BootParamsPath)
+	if err != nil {
+		return nil, aoserrors.Wrap(err)
+	}
+
+	paramsList := strings.Fields(string(data))
+
+	handler := &Handler{params: make(map[string]string)}
+
+	for _, param := range paramsList {
+		nameValue := strings.SplitN(param, "=", 2)
+
+		if len(nameValue) > 0 {
+			name := nameValue[0]
+			value := ""
+
+			if len(nameValue) > 1 {
+				value = nameValue[1]
+			}
+
+			handler.params[name] = value
+		}
+	}
+
+	return handler, nil
+}
+
+// GetBootParts returns boot partitions based on detect mode and partition suffixes.
+func (handler *Handler) GetBootParts(mode string, partSuffixes []string) ([]string, error) {
+	switch strings.ToLower(mode) {
+	case autoMode:
+		rootValue, ok := handler.params[rootParam]
+		if !ok {
+			return nil, aoserrors.Errorf("no %s parameter found", rootParam)
+		}
+
+		if !rootExp.MatchString(rootValue) {
+			return nil, aoserrors.New("can't define root device")
+		}
+
+		result := make([]string, len(partSuffixes))
+
+		for i, suffix := range partSuffixes {
+			result[i] = rootExp.ReplaceAllString(rootValue, suffix)
+		}
+
+		return result, nil
+
+	case bootArgsMode:
+		partA, ok := handler.params[aosUpdateBootPartA]
+		if !ok {
+			return nil, aoserrors.Errorf("no %s parameter found", aosUpdateBootPartA)
+		}
+
+		partB, ok := handler.params[aosUpdateBootPartB]
+		if !ok {
+			return nil, aoserrors.Errorf("no %s parameter found", aosUpdateBootPartB)
+		}
+
+		if partA == "" {
+			return nil, aoserrors.Errorf("%s parameter empty", aosUpdateBootPartA)
+		}
+
+		if partB == "" {
+			return nil, aoserrors.Errorf("%s parameter empty", aosUpdateBootPartB)
+		}
+
+		return []string{partA, partB}, nil
+
+	case manualMode:
+		result := make([]string, len(partSuffixes))
+
+		copy(result, partSuffixes)
+
+		return result, nil
+
+	default:
+		return nil, aoserrors.Errorf("unsupported mode: %s", mode)
+	}
+}
+
+// GetEnvPart returns boot environment partition based on detect mode and partition suffix.
+func (handler *Handler) GetEnvPart(mode string, partSuffix string) (string, error) {
+	switch strings.ToLower(mode) {
+	case autoMode:
+		rootValue, ok := handler.params[rootParam]
+		if !ok {
+			return "", aoserrors.Errorf("no %s parameter found", rootParam)
+		}
+
+		if !rootExp.MatchString(rootValue) {
+			return "", aoserrors.New("can't define root device")
+		}
+
+		return rootExp.ReplaceAllString(rootValue, partSuffix), nil
+
+	case bootArgsMode:
+		envPart, ok := handler.params[aosUpdateBootEnvPart]
+		if !ok {
+			return "", aoserrors.Errorf("no %s parameter found", aosUpdateBootEnvPart)
+		}
+
+		if envPart == "" {
+			return "", aoserrors.Errorf("%s parameter empty", aosUpdateBootEnvPart)
+		}
+
+		return envPart, nil
+
+	case manualMode:
+		return partSuffix, nil
+
+	default:
+		return "", aoserrors.Errorf("unsupported mode: %s", mode)
+	}
+}
---
+++ updatemodules/partitions/utils/bootparams/bootparams_test.go
@@ -0,0 +1,207 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright (C) 2023 Renesas Electronics Corporation.
+// Copyright (C) 2023 EPAM Systems, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bootparams_test
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/aoscloud/aos_updatemanager/updatemodules/partitions/utils/bootparams"
+)
+
+/***********************************************************************************************************************
+ * Init
+ **********************************************************************************************************************/
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{
+		DisableTimestamp: false,
+		TimestampFormat:  "2006-01-02 15:04:05.000",
+		FullTimestamp:    true,
+	})
+	log.SetLevel(log.DebugLevel)
+	log.SetOutput(os.Stdout)
+}
+
+/*******************************************************************************
+ * Main
+ ******************************************************************************/
+
+func TestMain(m *testing.M) {
+	tmpDir, err := os.MkdirTemp("", "um_")
+	if err != nil {
+		log.Fatalf("Error create temporary dir: %s", err)
+	}
+
+	bootparams.BootParamsPath = filepath.Join(tmpDir, "cmdline")
+
+	ret := m.Run()
+
+	if err := os.RemoveAll(tmpDir); err != nil {
+		log.Errorf("Error removing tmp dir: %s", err)
+	}
+
+	os.Exit(ret)
+}
+
+/***********************************************************************************************************************
+ * Tests
+ **********************************************************************************************************************/
+
+func TestBootParts(t *testing.T) {
+	type testData struct {
+		bootParams   string
+		mode         string
+		partSuffixes []string
+		result       []string
+		err          error
+	}
+
+	//nolint:goerr113
+	data := []testData{
+		{mode: "unknown", err: errors.New("unsupported mode: unknown")},
+		{mode: "auto", err: errors.New("no root parameter found")},
+		{mode: "auto", bootParams: "root=/dev/sda", err: errors.New("can't define root device")},
+		{mode: "bootargs", err: errors.New("no aosupdate.boot.parta parameter found ")},
+		{
+			mode: "bootargs", bootParams: "aosupdate.boot.parta",
+			err: errors.New("no aosupdate.boot.partb parameter found"),
+		},
+		{
+			mode: "bootargs", bootParams: "aosupdate.boot.parta aosupdate.boot.partb",
+			err: errors.New("aosupdate.boot.parta parameter empty"),
+		},
+		{
+			mode: "bootargs", bootParams: "aosupdate.boot.parta=/dev/sda1 aosupdate.boot.partb",
+			err: errors.New("aosupdate.boot.partb parameter empty"),
+		},
+		{
+			mode: "auto", bootParams: "root=/dev/sda3",
+			partSuffixes: []string{"1", "2"},
+			result:       []string{"/dev/sda1", "/dev/sda2"},
+		},
+		{
+			mode: "auto", bootParams: "root=/dev/mmcblk1p3",
+			partSuffixes: []string{"1", "2"},
+			result:       []string{"/dev/mmcblk1p1", "/dev/mmcblk1p2"},
+		},
+		{
+			mode: "bootargs", bootParams: "aosupdate.boot.parta=/dev/mmcblk0p1 aosupdate.boot.partb=/dev/mmcblk0p2",
+			result: []string{"/dev/mmcblk0p1", "/dev/mmcblk0p2"},
+		},
+		{
+			mode: "manual", partSuffixes: []string{"/dev/hda1", "/dev/hda2"},
+			result: []string{"/dev/hda1", "/dev/hda2"},
+		},
+	}
+
+	for _, item := range data {
+		if err := os.WriteFile(bootparams.BootParamsPath, []byte(item.bootParams), 0o600); err != nil {
+			t.Fatalf("Can't write boot params: %v", err)
+		}
+
+		params, err := bootparams.New()
+		if err != nil {
+			t.Fatalf("Can't create boot params: %v", err)
+		}
+
+		result, err := params.GetBootParts(item.mode, item.partSuffixes)
+
+		if item.err != nil {
+			if err == nil || !strings.Contains(err.Error(), item.err.Error()) {
+				t.Errorf("Unexpected error received: %v", err)
+			}
+		} else if err != nil {
+			t.Errorf("Can't get boot parts: %v", err)
+		}
+
+		if !reflect.DeepEqual(item.result, result) {
+			t.Errorf("Wrong result received: %v", result)
+		}
+	}
+}
+
+func TestEnvPart(t *testing.T) {
+	type testData struct {
+		bootParams string
+		mode       string
+		partSuffix string
+		result     string
+		err        error
+	}
+
+	//nolint:goerr113
+	data := []testData{
+		{mode: "unknown", err: errors.New("unsupported mode: unknown")},
+		{mode: "auto", err: errors.New("no root parameter found")},
+		{mode: "auto", bootParams: "root=/dev/sda", err: errors.New("can't define root device")},
+		{mode: "bootargs", err: errors.New("no aosupdate.boot.envPart parameter found ")},
+		{
+			mode: "bootargs", bootParams: "aosupdate.boot.envPart",
+			err: errors.New("aosupdate.boot.envPart parameter empty"),
+		},
+		{
+			mode: "auto", bootParams: "root=/dev/sda3",
+			partSuffix: "3",
+			result:     "/dev/sda3",
+		},
+		{
+			mode: "auto", bootParams: "root=/dev/mmcblk1p3",
+			partSuffix: "3",
+			result:     "/dev/mmcblk1p3",
+		},
+		{
+			mode: "bootargs", bootParams: "aosupdate.boot.envPart=/dev/mmcblk0p3",
+			result: "/dev/mmcblk0p3",
+		},
+		{
+			mode: "manual", partSuffix: "/dev/hda3", result: "/dev/hda3",
+		},
+	}
+
+	for _, item := range data {
+		if err := os.WriteFile(bootparams.BootParamsPath, []byte(item.bootParams), 0o600); err != nil {
+			t.Fatalf("Can't write boot params: %v", err)
+		}
+
+		params, err := bootparams.New()
+		if err != nil {
+			t.Fatalf("Can't create boot params: %v", err)
+		}
+
+		result, err := params.GetEnvPart(item.mode, item.partSuffix)
+
+		if item.err != nil {
+			if err == nil || !strings.Contains(err.Error(), item.err.Error()) {
+				t.Errorf("Unexpected error received: %v %v", err, item)
+			}
+		} else if err != nil {
+			t.Errorf("Can't get env part: %v", err)
+		}
+
+		if !reflect.DeepEqual(item.result, result) {
+			t.Errorf("Wrong result received: %v", result)
+		}
+	}
+}
