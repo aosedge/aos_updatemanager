b3e15d3e6de10bb62e050bc9eea1b43fc13e504f
Mykola Kobets <mykola_kobets@epam.com>
2024-10-17 13:21:33 +0300

    [umclient] UM client renew certificates adaptation
    Signed-off-by: Mykola Kobets <mykola_kobets@epam.com>

--- updatehandler/updatehandler_test.go
+++ updatehandler/updatehandler_test.go
@@ -260,6 +260,7 @@ func TestUpdate(t *testing.T) {
 	if handler, err = updatehandler.New(cfg, storage, storage); err != nil {
 		t.Fatalf("Can't create update handler: %s", err)
 	}
+
 	defer handler.Close()
 
 	testOperation(t, handler, handler.Registered, &newStatus,
@@ -826,6 +827,7 @@ func TestVersionInUpdate(t *testing.T) {
 	if handler, err = updatehandler.New(cfg, storage, storage); err != nil {
 		t.Fatalf("Can't create update handler: %s", err)
 	}
+
 	defer handler.Close()
 
 	testOperation(t, handler, handler.Registered, &newStatus,
